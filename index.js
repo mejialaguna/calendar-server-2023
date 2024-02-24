93c3e08ad0febb7b76a35e12fb8fc6f5e9e86732
Jose Luis Mejia Laguna <jlml@joses-mbp.mynetworksettings.com>
2024-02-24 15:11:30 -0500

    refactoring severside index file

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-const express = require("express"); // !step 1 install express dependencies
-const cors = require("cors");
-require("dotenv").config(); // ! step 3 install dotenv dependencies
-
+const express = require("express");
+require("dotenv").config(); 
+const cors = require('cors');
 const { dbConnection } = require("./database/config");
 
-const startServer = async () => {
-  const app = express(); // !step 1
-  const PORT = process.env.PORT || 3000; // ! step 3 also add .env file and add env variables there.
+// Crear el servidor de express
+const app = express();
 
-  // !!Add CORS middleware here if need or this middleware will just have default settings.
-  app.use(cors());
+// Base de datos
+dbConnection();
 
-  // ! public index file path
-  app.use(express.static("public")); // ! step 4 use app.use and point to public folder.
+// CORS
+app.use(cors())
 
-  app.use(express.json()); // ! step 5 use app.use to parse json data. (process the content of the body)
+// Directorio Público
+app.use( express.static('public') );
 
-  app.use(require("./routes")); // !step 5 use point to the routes folder were the routes are stored.
+// Lectura y parseo del body
+app.use( express.json() );
 
-  // TODO: step 6 add routes
+// Rutas
+app.use(require('./routes')); 
 
-  dbConnection(); // TODO call the db connection.
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`); // ! all of this app listen step 2
-  });
-};
-
-startServer();
+// Escuchar peticiones
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+});
